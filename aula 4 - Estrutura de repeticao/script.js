0f860f27beb8e07d01ea2967a9b08f08f0e783aa
Pereira2013 <robsonwppereira2013@gmail.com>
2022-03-31 08:30:54 -0300

    Estrutura de Repetição

---
+++ aula 4 - Estrutura de repeticao/script.js
@@ -0,0 +1,45 @@
+let array = ['Valor1', 'Valor2', 'Valor3', 'Valor4', 'Valor5'];
+
+let object = { propriedade1: 'Valor1', propriedade2: 'Valor2', propriedade3: 'Valor3' };
+
+// for - executa uma instrução até que ela seja falsa
+
+// for (let index = 0; index < array.length; index++) {
+//     console.log(index);    
+// }
+
+// // for/in executa retição a aprtir de uma propriedade
+// // com array 
+
+// for (let i in array) {
+//     console.log(i);
+// }
+// // com object
+// for (i in object) {
+//     console.log(i);
+// }
+
+// // for/of - executa repetição a partir do valor 
+// // comm array
+
+// for (i of array) {
+//     console.log(i);
+// }
+
+// // não usual em object
+// for (i of object.propriedade1) {
+//     console.log(i);
+// }
+
+// WHILE
+
+var a = 0;
+// while (a < 10) {
+//     a++;
+//     console.log(a);
+// }
+
+do {
+    a++;
+    console.log(a);
+} while (a < 10);
